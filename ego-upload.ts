a91da94c824512ec3ca59a3ec7c1f9886436ad2b
Sebastian Wiesner <sebastian@swsnr.de>
2024-08-24 11:52:50 +0200

    Use jsr imports

--- ego-upload.ts
+++ ego-upload.ts
@@ -4,17 +4,17 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { basename, extname } from "https://deno.land/std@0.206.0/path/mod.ts";
+import { basename, extname } from "jsr:std/path@1";
 
 import {
   Confirm,
   Input,
   Secret,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.3/prompt/mod.ts";
+} from "jsr:cliffy/prompt@1.0.0-rc.5";
 import {
   Command,
   CompletionsCommand,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+} from "jsr:cliffy/command@v1.0.0-rc.5";
 
 const VERSION = "1.2.0";
 
